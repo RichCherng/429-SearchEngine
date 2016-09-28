6329f48fcfa68711169a53029db6bb8d282a290e
Rich Cherngchaosil <richer_po@hotmail.com>
2016-09-28 14:05:45 -0700

    Added PorterStemmer, DocumentReader class

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,59 @@
-import java.util.ArrayList;
-
 public class Main {
 
 
 	public static void main(String[] args){
 
-		JSONSIFY parseJSON = new JSONSIFY("all-nps-sites.json");
-		Document doc = parseJSON.read();
+//		JSONSIFY parseJSON 	= new JSONSIFY("all-nps-sites.json");
+//		Document doc 		= parseJSON.read();
+//
+//		ArrayList<Document.Article> document = new ArrayList<Document.Article>();
+//
+//		ArrayList<Document.Article> articles = doc.getDocument();
 
-		ArrayList<Document.Article> articles = doc.getDocument();
 
-		/** For purpose of developing **/
-		for(Document.Article a: articles){
-			String title 	= a.title;
-			String url 		= a.url;
-			String body 	= a.body;
 
-			TokenStream scan = new TokenStream(a.body);
+//		/** For purpose of developing **/
+//		for(Document.Article a: articles){
+//			String title 	= a.title;
+//			String url 		= a.url;
+//			String body 	= a.body;
+//
+//			TokenStream scan = new TokenStream(a.body);
+//			while(scan.hasNextToken()){
+//				System.out.print(scan.nextToken() + ',');
+//			}
+//
+//
+//
+//			break;
+//		}
+
+		DocumentReader docReader = new DocumentReader();
+		PorterStemmer stemmer = new PorterStemmer();
+		PositionalInvertedIndex PII = new PositionalInvertedIndex();
+
+		docReader.read("all-nps-sites.json");
+		int num_doc = docReader.size();
+		for(int id = 0; id < num_doc; id++){
+
+			Document.Article article = docReader.get(id);
+			TokenStream scan = new TokenStream(article.body);
+			int positionIndex = 0;
 			while(scan.hasNextToken()){
-				System.out.print(scan.nextToken() + ',');
+				String token = scan.nextToken();
+
+				// Check if token contain hypen
+				if(token.contains("-")){
+
+				} else {
+
+
+				}
+
+				System.out.print(token + ":");
+
+				positionIndex++;
 			}
-
-
-
 			break;
 		}
 	}
--- src/PositionalInvertedIndex.java
+++ src/PositionalInvertedIndex.java
@@ -2,8 +2,17 @@ import java.util.HashMap;
 
 public class PositionalInvertedIndex {
 
-	private HashMap<String, String> mIndex;
+	private HashMap<String, Posting> mIndex;
+
 	public PositionalInvertedIndex(){
+		mIndex = new HashMap<String, Posting>();
+	}
+
+	public void addTerm(String term, int documentID, int position){
+
+	}
+
+	class Posting{
 
 	}
 }
