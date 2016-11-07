9b87e3b4366959fb214c49aebe08caa677246c3f
Rich Cherngchaosil <richer_po@hotmail.com>
2016-11-06 18:31:30 -0800

    Modify KGram to use HashSet

--- src/KGramIndex.java
+++ src/KGramIndex.java
@@ -1,24 +1,24 @@
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 @SuppressWarnings("serial")
 public class KGramIndex implements Serializable {
 
-	private HashMap<String, ArrayList<String>> oneGram;
-	private HashMap<String, ArrayList<String>> twoGram;
-	private HashMap<String, ArrayList<String>> threeGram;
+	private HashMap<String, HashSet<String>> oneGram;
+	private HashMap<String, HashSet<String>> twoGram;
+	private HashMap<String, HashSet<String>> threeGram;
 
 	public KGramIndex(){
-		oneGram		= new HashMap<String, ArrayList<String>>();
-		twoGram 	= new HashMap<String, ArrayList<String>>();
-		threeGram 	= new HashMap<String, ArrayList<String>>();
+		oneGram		= new HashMap<String, HashSet<String>>();
+		twoGram 	= new HashMap<String, HashSet<String>>();
+		threeGram 	= new HashMap<String, HashSet<String>>();
 	}
 
 	public void addType(String type){
 		char[] cArr = type.toCharArray();
 		for(int i = 0 ; i < cArr.length; i++){
-			addToHashMap(oneGram, cArr[i]+"", type);
+			addToHashMap(oneGram, cArr[i]+"", type); // Add to one-gram
 			if(i == 0){
 				/** Starting **/
 
@@ -47,27 +47,16 @@ public class KGramIndex implements Serializable {
 		}
 	}
 
+	public HashSet<String> getPossibleList(String pWord){
 
-//	public void test(){
-//		for(Map.Entry<String, ArrayList<String>> entry: twoGram.entrySet()){
-//			System.out.println(entry.getKey());
-//			for(String s : entry.getValue()){
-//				System.out.println("\t" + s);
-//			}
-//		}
-//		for(Map.Entry<String, ArrayList<String>> entry: threeGram.entrySet()){
-//			System.out.println(entry.getKey());
-//			for(String s : entry.getValue()){
-//				System.out.println("\t" + s);
-//			}
-//		}
-//	}
+		return null;
+	}
 
 
-	private void addToHashMap(HashMap<String, ArrayList<String>> hm, String gram, String type){
+	private void addToHashMap(HashMap<String, HashSet<String>> hm, String gram, String type){
 		if(hm.containsKey(gram)){
 			// Contain in the map
-			ArrayList<String> words = hm.get(gram);
+			HashSet<String> words = hm.get(gram);
 			if(!words.contains(type)){
 				// Not already exists
 				words.add(type);
@@ -75,7 +64,7 @@ public class KGramIndex implements Serializable {
 		} else {
 			// Not alredy in the map
 			// Create new list
-			ArrayList<String> words = new ArrayList<String>();
+			HashSet<String> words = new HashSet<String>();
 			words.add(type);
 			hm.put(gram, words);
 		}
