72524ced3365e1dc9c31673c2cc530d66b9eb142
Rich Cherngchaosil <richer_po@hotmail.com>
2016-11-08 01:36:44 -0800

    clean up

--- src/Main.java
+++ src/Main.java
@@ -47,70 +47,17 @@ public class Main {
 					aSC = new SpellingCorrection(aKGI); // Initialize Spelling Correction
 					in.close();
 				} catch (IOException e) {
-//					e.printStackTrace();
 					System.out.println("Failed Processing Serialized files");
 				} catch (ClassNotFoundException e) {
-//					e.printStackTrace();
 					System.out.println("Failed Processing Serialized files");
 				}
 				System.out.println("Loading index completed");
-
-
-				RankRetrievalsObject mRRO = new RankRetrievalsObject(aDII);
-//				System.out.println(aDII.getDocListPosting("dog"));
-//				System.out.println(aDII.getDocListPosting("cat"));
-				mRRO.processQuery("dog cat");
-				// Start parsing queries
 				QueryParser querie =  new QueryParser(aDII, aBI, aSC);
 				querie.richRun();
 
 				break;
 		}
 
-
-
-//		PositionalInvertedIndex aPII 		= new PositionalInvertedIndex();
-//		BiwordIndex aBI 					= new BiwordIndex();
-//		DocumentReader docReader 			= new DocumentReader(aPII, aBI);
-//		DirectoryParser aDirectoryParser 	= new DirectoryParser(docReader);
-////
-//		// Store each articles in the ArrayList of Document.Article
-//		aDirectoryParser.parseDirectory(dir);
-
-
-		// Check Dictionary
-//		for(String term: aPII.getSortedListOfVocab()){
-//			aPII.getListOfPosting(term);
-//			if(aPII.getListOfPosting(term) == null){
-//				System.out.println(term);
-//			}
-//
-//		}
-
-//		/*** Write To Disk ****/
-//		IndexWriter diskWriter = new IndexWriter();
-//		diskWriter.writeToDisk(dir,aPII);
-//
-//		/*** Read from Disk ***/
-//		DiskInvertedIndex aDII = new DiskInvertedIndex(dir);
-//
-//
-//		// compare
-//		Posting[] fromDisk = aDII.getPostings(PorterStemmer.processToken("park"));
-//		Posting[] fromIndex = aPII.getListOfPosting(PorterStemmer.processToken("park"));
-//		for(int i = 0; i < fromDisk.length; i++){
-////			System.out.println(fromDisk[i]);
-////			System.out.println(fromIndex[i]);
-//		}
-//
-//		Pair<Integer>[] pairs= aDII.getDocList(PorterStemmer.processToken("park"));
-//		for(Pair<Integer> p: pairs){
-//			System.out.println(p.getFirst() + " : " + p.getSecond());
-//		}
-
-		// Create Positional Inverted Index from the list of articles
-//		QueryParser querie =  new QueryParser(docReader, aPII, aBI, aDirectoryParser);
-//		querie.leafRun();
 
 	}
 
